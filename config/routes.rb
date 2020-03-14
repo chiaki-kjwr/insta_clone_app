d980f7f7f1e68ed7fdcd2eed2050bf3bbd964ed6
EC2 Default User <ec2-user@ip-172-31-38-144.ap-northeast-1.compute.internal>
2020-03-14 13:53:58 +0000

    Add posts_new_page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users,
    controllers: { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'pages#home'
-get '/users/:id', to: 'users#show', as: 'user'
+  root 'pages#home'
+  get '/users/:id', to: 'users#show', as: 'user'
+
+  resources :posts, only: %i(new create) do
+    resources :photos, only: %i(create)
+  end
 end
