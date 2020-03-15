778aa7bf55bfc4d4d00362f5a34d9dc7d85e5d5f
EC2 Default User <ec2-user@ip-172-31-38-144.ap-northeast-1.compute.internal>
2020-03-15 22:15:32 +0000

    Fix logo_image

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
    controllers: { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root 'posts#index'
+  root 'pages#home'
   get '/users/:id', to: 'users#show', as: 'user'
 
   resources :posts, only: %i(index new create show destroy) do
