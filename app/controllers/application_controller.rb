c6dc18b31a7b14e530dfb7d41662af8f88999c2a
EC2 Default User <ec2-user@ip-172-31-38-144.ap-northeast-1.compute.internal>
2020-03-15 14:01:10 +0000

    Fix user_edit_page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,8 @@ protect_from_forgery with: :exception
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:username])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name,:username])
+      added_attrs = [:name, :email,:number,:web,:sex,:profile]
+      devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
+      devise_parameter_sanitizer.permit(:account_update, keys: added_attrs)
     end
 end
