8d60df5c29ee81e35886e6f851399f225a1b6e25
EC2 Default User <ec2-user@ip-172-31-38-144.ap-northeast-1.compute.internal>
2020-03-18 23:14:51 +0000

    Add Relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,23 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes
   has_many :comments
-  
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+                                  
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy                                
+                                  
+  has_many :following, through: :active_relationships, source: :followed                                
+  has_many :followers, through: :passive_relationships, source: :follower
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
          validates :name, presence: true, length: { maximum: 20 }
          validates :username, presence: true, length: { maximum: 20 }
+  
          
   def update_without_current_password(params, *options)
     params.delete(:current_password)
@@ -23,4 +34,17 @@ class User < ApplicationRecord
     clean_up_passwords
     result
   end
+  
+  def follow(other_user)
+    following << other_user
+  end
+  
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+  
+  def following?(other_user)
+    following.include?(other_user)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'pages#home'
+  resources :users,only: %i(show destroy) do
+    member do
+      get :following, :followers
+    end
   get '/users/:id', to: 'users#show', as: 'user'
-  delete 'users/:id', to:'users#destroy'
+  delete 'users/:id', to:'users#destroy' 
+  end
+  
+  
   resources :posts, only: %i(index show new create destroy) do
     resources :photos, only: %i(create)
     
@@ -13,4 +20,6 @@ Rails.application.routes.draw do
     
     resources :comments, only: %i(create destroy)
   end
+  
+  resources :relationships,       only: [:create, :destroy]
 end
