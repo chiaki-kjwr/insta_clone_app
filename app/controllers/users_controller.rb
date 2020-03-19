8e61d0db02fdc641a6cfd7c659add0b07299a487
EC2 Default User <ec2-user@ip-172-31-38-144.ap-northeast-1.compute.internal>
2020-03-19 13:45:14 +0000

    Add follow stats

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,19 @@ class UsersController < ApplicationController
     redirect_to("/")
    end
    
+    def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following
+    render 'show_follow'
+    end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follow'
+  end
+   
    
 end
