5832cafb653c7612b7f6181e11f890ba644a8d19
EC2 Default User <ec2-user@ip-172-31-38-144.ap-northeast-1.compute.internal>
2020-03-20 09:09:59 +0000

    Fix index_page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     if @post.photos.present?
       @post.save
-      redirect_to "/posts"
+      redirect_to  "/posts/#{@post.id}"
       flash[:notice] = "投稿が保存されました"
     else
       redirect_to root_path
